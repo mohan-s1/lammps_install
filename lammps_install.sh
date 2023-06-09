546fb61a4e35af225192a3d506961f98f4067f97
Mohan <118559061+mohan-s1@users.noreply.github.com>
2023-06-09 10:10:20 -0400

    Add files via upload

---
+++ lammps_install.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+echo "Updating apps native to Ubuntu"
+
+sudo apt update
+
+sudo apt upgrade 
+
+echo "Installing Git and CMake if not already present"
+
+sudo apt-get update
+
+sudo apt-get upgrade
+
+sudo apt-get install git 
+
+sudo apt-get install cmake
+
+git clone -b release https://github.com/lammps/lammps.git mylammps
+
+cd mylammps
+
+mkdir build; cd build
+
+cmake ../cmake -D PKG_MOLECULE=yes -D PKG_KSPACE=yes -D PKG_RIGID=yes -D PKG_MANYBODY=yes -D PKG_REAXFF=yes 
+
+cmake --build .
+
