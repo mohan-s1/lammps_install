b121f1596f103bdb9c81ea952949fdbd9eb2c15b
Mohan <118559061+mohan-s1@users.noreply.github.com>
2023-06-09 10:14:07 -0400

    Update lammps_install.sh

--- lammps_install.sh
+++ lammps_install.sh
@@ -22,7 +22,7 @@ cd mylammps
 
 mkdir build; cd build
 
-cmake ../cmake -D PKG_MOLECULE=yes -D PKG_KSPACE=yes -D PKG_RIGID=yes -D PKG_MANYBODY=yes -D PKG_REAXFF=yes 
+cmake ../cmake -D PKG_MOLECULE=yes -D PKG_KSPACE=yes -D PKG_RIGID=yes -D PKG_MANYBODY=yes -D PKG_REAXFF=yes -D PKG_MANYBODY
 
 cmake --build .
 
