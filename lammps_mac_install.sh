98304868a6a8844529d199cc5a1a591d6e52164e
Mohan <118559061+mohan-s1@users.noreply.github.com>
2023-06-09 18:19:31 -0400

    Update lammps_mac_install.sh

--- lammps_mac_install.sh
+++ lammps_mac_install.sh
@@ -1,1 +1,19 @@
+#!/bin/bash
 
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+brew install cmake
+
+brew install git
+
+brew install open-mpi
+
+git clone -b release https://github.com/lammps/lammps.git mylammps
+
+cd mylammps
+
+mkdir build; cd build
+
+cmake ../cmake -D PKG_MOLECULE=yes -D PKG_KSPACE=yes -D PKG_RIGID=yes -D PKG_MANYBODY=yes -D PKG_REAXFF=yes -D PKG_MANYBODY=yes -D PKG_MC=yes
+
+cmake --build .
